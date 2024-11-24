bfd311276f3ca62d43221af86fbb430dd18e392b
LINK_KI_DUNIYA <158588158+ROHAN-LKD-WORLD@users.noreply.github.com>
2024-11-24 17:18:53 +0530

    config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("9303922"))
+API_HASH = getenv("a17677495aa9ae010b897c2d65146282")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN = getenv("6281318666:AAH1JmtLA-QXVU8Kzue3GeYTx13hCSspqds")
+OWNER_ID = int(getenv("7385703361"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI = getenv("mongodb+srv://sukoon:sukoon@cluster0.fldi6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MUST_JOIN = getenv("LINK_KI_DUNIYA", None)
